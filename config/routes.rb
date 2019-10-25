7ec0932e22a7377559a8ff579ac4f80b95ab926b
aokyut <epaperamic@gmail.com>
2019-10-25 14:16:56 +0900

    integrate source css/html into app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home' => "static_pages#index"
+  root "static_pages#home"
+  get "/index" => "static_pages#index"
+  get "/article" =>"static_pages#article"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
