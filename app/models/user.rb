1c58b05bdf1b13d0eade066e6623a880b1485d84
aokyut <epaperamic@gmail.com>
2019-10-26 23:48:15 +0900

    make users name and email validation test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name,presence:true,length:{maximum:50}
+  regulex=/\A[\w+\-.]+@[a-z\d\-.]*[a-z\d]+\.[a-z]+\z/
+  validates :email,presence:true,length:{maximum:255},format:{with:regulex},uniqueness:{case_sensitive:false}
+
 end
+  
