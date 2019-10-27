4c4579366ce679839e899e320d752be7d78fca52
aokyut <epaperamic@gmail.com>
2019-10-27 17:50:48 +0900

    make sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :name,presence:true,length:{maximum:50}
   regulex=/\A[\w+\-.]+@[a-z\d\-.]*[a-z\d]+\.[a-z]+\z/
   validates :email,presence:true,length:{maximum:255},format:{with:regulex},uniqueness:{case_sensitive:false}
-  validates :password,presence:true,length:{minimum:6}
+  validates :password,presence:true,length:{minimum:6},allow_nil:true
 
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/index" => "static_pages#index"
   get "/article" =>"static_pages#article"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
